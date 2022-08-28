703a654de10c3deaa88034b06fe9c9a73568ca94
Alterlapsus <israel.delen96@gmail.com>
2022-08-27 18:14:40 -0600

    Se crea la tabla alumno y sus filas y columnas

--- database/migrations/2022_08_27_180458_alumnos.php
+++ database/migrations/2022_08_27_180458_alumnos.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class Alumnos extends Migration
-{
-    /**
+{   /**
      * Run the migrations.
      *
      * @return void
