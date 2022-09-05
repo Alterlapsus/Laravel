c24210380a21468aa8f2f2a397aa20446f0c9e64
Alterlapsus <israel.delen96@gmail.com>
2022-09-04 19:56:32 -0600

    Se modifica el card body

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card"  style="background-color: #fffa;">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header" style="font-size: 35px;">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
+                <div class="card-body" style="font-size: 45px;">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
