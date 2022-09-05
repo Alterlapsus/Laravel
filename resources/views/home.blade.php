f95b8739de68c85575b8a2005c5ad8ce731bcdef
Alterlapsus <israel.delen96@gmail.com>
2022-09-04 19:53:14 -0600

    se cambia color a label

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card"  style="background-color: #fffa;">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
